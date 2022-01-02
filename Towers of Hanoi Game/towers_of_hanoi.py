d7da1a7eee213292dd23b5a1f48c1fce5e5e98a8
Abdulrahman Qadi <71637340+Abdulrahman-Qadi@users.noreply.github.com>
2022-01-02 23:42:43 +0000

    Add files via upload

---
+++ Towers of Hanoi Game/towers_of_hanoi.py
@@ -0,0 +1,285 @@
+# Doubly Linked List
+class Node:
+  def __init__(self, value, next_node=None, prev_node=None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
+
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head_node = None
+    self.tail_node = None
+  
+  def add_to_head(self, new_value):
+    new_head = Node(new_value)
+    current_head = self.head_node
+
+    if current_head != None:
+      current_head.set_prev_node(new_head)
+      new_head.set_next_node(current_head)
+
+    self.head_node = new_head
+
+    if self.tail_node == None:
+      self.tail_node = new_head
+
+  def add_to_tail(self, new_value):
+    new_tail = Node(new_value)
+    current_tail = self.tail_node
+
+    if current_tail != None:
+      current_tail.set_next_node(new_tail)
+      new_tail.set_prev_node(current_tail)
+
+    self.tail_node = new_tail
+
+    if self.head_node == None:
+      self.head_node = new_tail
+
+  def remove_head(self):
+    removed_head = self.head_node
+
+    if removed_head == None:
+      return None
+
+    self.head_node = removed_head.get_next_node()
+
+    if self.head_node != None:
+      self.head_node.set_prev_node(None)
+
+    if removed_head == self.tail_node:
+      self.remove_tail()
+
+    return removed_head.get_value()
+
+  def remove_tail(self):
+    removed_tail = self.tail_node
+
+    if removed_tail == None:
+      return None
+
+    self.tail_node = removed_tail.get_prev_node()
+
+    if self.tail_node != None:
+      self.tail_node.set_next_node(None)
+
+    if removed_tail == self.head_node:
+      self.remove_head()
+
+    return removed_tail.get_value()
+
+  def remove_by_value(self, value_to_remove):
+    node_to_remove = None
+    current_node = self.head_node
+
+    while current_node != None:
+      if current_node.get_value() == value_to_remove:
+        node_to_remove = current_node
+        break
+
+      current_node = current_node.get_next_node()
+
+    if node_to_remove == None:
+      return None
+
+    if node_to_remove == self.head_node:
+      self.remove_head()
+    elif node_to_remove == self.tail_node:
+      self.remove_tail()
+    else:
+      next_node = node_to_remove.get_next_node()
+      prev_node = node_to_remove.get_prev_node()
+      next_node.set_prev_node(prev_node)
+      prev_node.set_next_node(next_node)
+
+    return node_to_remove
+
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.head_node
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+
+
+
+# Queues
+class Queue:
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
+    self.size = 0
+    
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
+    else:
+      print("Sorry, no more room!")
+         
+  def dequeue(self):
+    if self.get_size() > 0:
+      item_to_remove = self.head
+      print(str(item_to_remove.get_value()) + " is served!")
+      if self.get_size() == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("The queue is totally empty!")
+  
+  def peek(self):
+    if self.size > 0:
+      return self.head.get_value()
+    else:
+      print("No orders waiting!")
+  
+  def get_size(self):
+    return self.size
+  
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    else:
+      return self.max_size > self.get_size()
+    
+  def is_empty(self):
+    return self.size == 0
+
+
+
+# Stacks
+class Stack:
+  def __init__(self, name):
+    self.size = 0
+    self.top_item = None
+    self.limit = 1000
+    self.name = name
+  
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+    else:
+      print("No more room!")
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    print("This stack is totally empty.")
+
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+  
+  def get_size(self):
+    return self.size
+  
+  def get_name(self):
+    return self.name
+  
+  def print_items(self):
+    pointer = self.top_item
+    print_list = []
+    while(pointer):
+      print_list.append(pointer.get_value())
+      pointer = pointer.get_next_node()
+    print_list.reverse()
+    print("{0} Stack: {1}".format(self.get_name(), print_list))
+
+
+#Towers of Hanoi
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while num_disks < 3:
+  num_disks = int(input("Enter a number greater than or equal to 3\n"))
+for num in range(num_disks, 0, -1):
+  left_stack.push(num)
+  num_optimal_moves = 2**(num_disks - 1)
+print("\nThe fastest you can solve this game is in " +str(num_optimal_moves)+ " moves")
+#Get User Input
+def get_input():
+  choices = [stack.get_name()[0] for stack in stacks]
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+      print("Enter ", letter, " for ", name)
+    user_input = input("")
+    if user_input in choices:
+      for i in range(len(stacks)):
+        if user_input == choices[i]:
+          return stacks[i]
+        
+#Play the Game
+num_user_moves = 0
+while right_stack.get_size() != num_disks:
+  print("\n\n\n...Current Stacks...")
+  for i in range(len(stacks)):
+    stacks[i].print_items()
+  while True:
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.is_empty():
+      print("\n\nInvalid Move. Try Again")
+    elif to_stack.is_empty() or from_stack.peek() < to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print("\n\nInvalid Move. Try Again")
+  print("\n\nYou completed the game in", num_user_moves, "moves, and the optimal number of moves is", num_optimal_moves)
